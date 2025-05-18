466dee7abe5dab2167586a634e1ab6b7896d2b24
tutonmondal <tutonmondal227@gmail.com>
2025-05-18 09:07:51 +0530

    Hello world! comment added

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,12 @@ void main() {
     ),
   );
 }
+
+/* This is the flutter hello world application, the app start or began from the
+ * main function "void main()" and inside the main function we have the runApp()
+ * function which take any widget and place that widget into the root of the
+ * widget tree. So in this case the widget tree consist of two widgets Center
+ * and Text widget, and the flutter framework force the root widget to cover the
+ * entire screen, so the "Hello world!" will cover the entire screen.
+ */
+
