ccbd2bb04fa8752935703f90474c248a20018f34
tutonmondal <tutonmondal227@gmail.com>
2025-05-18 10:00:46 +0530

    Basic widgets with comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,68 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        'Hello World!',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.blue),
-      ),
+    MaterialApp(
+      title: 'My App', // Used by OS task switcher
+      home: SafeArea(child: MyScaffold()),
     ),
   );
 }
 
-/* This is the flutter hello world application, the app start or began from the
- * main function "void main()" and inside the main function we have the runApp()
- * function which take any widget and place that widget into the root of the
- * widget tree. So in this case the widget tree consist of two widgets Center
- * and Text widget, and the flutter framework force the root widget to cover the
- * entire screen, so the "Hello world!" will cover the entire screen.
- */
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    // Material is conceptual piece of paper
+    // on which the UI appears
+    return Material(
+      // Column is a vertical linear layout
+      child: Column(
+        children: [
+          MyAppBar(
+            title: Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.titleLarge,
+            ),
+          ),
+          const Expanded(child: Center(child: Text('Hello World!'))),
+        ],
+      ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, super.key});
+
+  // Field variables in a widget subclass are always marked as final
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        // Row is a horizontal liner layout
+        children: [
+          IconButton(
+            onPressed: null, // null disable the button
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+          ),
+          // Expanded expands it's child
+          // to fill the available space
+          Expanded(child: title),
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+          ),
+        ],
+      ),
+    );
+  }
+}
