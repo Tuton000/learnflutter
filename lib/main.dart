56faf1646d95bef4deacb8b2e811486b106d3f80
tutonmondal <tutonmondal227@gmail.com>
2025-05-18 10:46:36 +0530

    Using material components

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'My App', // Used by OS task switcher
-      home: SafeArea(child: MyScaffold()),
-    ),
-  );
+  runApp(MaterialApp(title: 'Flutter Tutorial', home: FlutterTutorial()));
 }
 
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
+class FlutterTutorial extends StatelessWidget {
+  const FlutterTutorial({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Material is conceptual piece of paper
-    // on which the UI appears
-    return Material(
-      // Column is a vertical linear layout
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.titleLarge,
-            ),
-          ),
-          const Expanded(child: Center(child: Text('Hello World!'))),
-        ],
-      ),
-    );
-  }
-}
-
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, super.key});
-
-  // Field variables in a widget subclass are always marked as final
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        // Row is a horizontal liner layout
-        children: [
-          IconButton(
-            onPressed: null, // null disable the button
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-          ),
-          // Expanded expands it's child
-          // to fill the available space
-          Expanded(child: title),
+    // Scaffold is a layout for the major Material components
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: null,
+          tooltip: 'Navigation menu',
+          icon: Icon(Icons.menu),
+        ),
+        title: Text('Example Title'),
+        actions: [
           IconButton(
             onPressed: null,
-            icon: Icon(Icons.search),
             tooltip: 'Search',
+            icon: Icon(Icons.search),
           ),
         ],
+      ),
+      // body is the majority of the screen
+      body: const Center(child: Text('Hello world!')),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        onPressed: null,
+        child: Icon(Icons.add),
       ),
     );
   }
